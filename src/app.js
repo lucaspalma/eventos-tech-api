7a9a37da0d09ef908a59d3143a20f2b8255e5f08
Matheus Brandino <m.v.brandino@gmail.com>
2017-07-14 16:50:00 -0300

    Added monk and create a route for get data from mongo

--- src/app.js
+++ src/app.js
@@ -1,23 +1,7 @@
 const express = require('express')
+const db = require('monk')('localhost/events')
 const app = express()
 const port = 3000
-const allEvents = [
-  {
-    name: 'QConSP',
-    data: ['24/04/2017', '25/04/2017', '26/04/2017'],
-    link: 'http://qconsp.com'
-  },
-  {
-    name: 'FrontInSampa',
-    data: ['01/07/2017'],
-    link: 'http://www.frontinsampa.com.br'
-  },
-  {
-    name: 'FrontInVale',
-    data: ['16/07/2017'],
-    link: 'https://www.eventick.com.br/frontinvale2017'
-  }
-]
 
 
 app.use((req, res, next) => {
@@ -26,11 +10,18 @@ app.use((req, res, next) => {
 })
 
 app.get('/allevents', (req, res) => {
-  res.json(allEvents);
-})
+
+  const events = db.get('events');
+
+  events.find({},{_id:false}).then((result)=> {
+   res.json(result);
+  });
+
+ 
+});
 
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`)
   console.log('To down the server: ctrl + c')
-})
+});
