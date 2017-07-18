d659835a33479112dc618b1645b2dc9b0457b660
MarcoBrunoBR <marco.bruno.br@gmail.com>
2017-07-18 00:50:13 -0300

    Altered the routes and created postman file

--- src/routes/events.js
+++ src/routes/events.js
@@ -1,8 +1,9 @@
 const EventsController = require('../controller/EventsController')
 
 module.exports = (app) => {
-  app.get('/allevents', EventsController.getAll.bind(EventsController))
-  app.get('/allevents/:tag', EventsController.getAllInATag.bind(EventsController))
-  app.get('/next', EventsController.getNext.bind(EventsController))
-  app.get('/next/:tag', EventsController.getNextInTag.bind(EventsController))
+  app.get('/all-next-events', EventsController.getAll.bind(EventsController))
+  app.get('/all-next-events/:tag', EventsController.getAllInATag.bind(EventsController))
+
+  app.get('/next-event', EventsController.getNext.bind(EventsController))
+  app.get('/next-event/:tag', EventsController.getNextInTag.bind(EventsController))
 }
