77795bc61b6e19abdb5b1f852388d876069b7ab3
MarcoBrunoBR <marco.bruno.br@gmail.com>
2017-08-07 22:35:55 -0300

    Added env variable to mongodb

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
+const DB_URL = process.env.DB_URL
+const DB_NAME = process.env.DB_NAME
 
-mongoose.connect('mongodb://localhost/eventos-tech-api')
+mongoose.connect(`mongodb://${DB_URL}/${DB_NAME}`)
 
 mongoose.connection.on('connected', () => {
   console.log('Connected in MongoDB')
